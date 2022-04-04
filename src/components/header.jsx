013b345274f66eb5b8cc0e5d5a312db129b218a7
Terry <terry.grevillot@utt.fr>
2022-04-05 00:00:51 +0200

    Swap logo Intro particle features implemented

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,3 @@
-import ParticlesBg from "particles-bg";
 import useWindowSize from "@rooks/use-window-size";
 import ParticleImage, {
   ParticleOptions,
@@ -15,7 +14,7 @@ const particleOptions = {
     // Make a particle for this pixel if blue > 50 (range 0-255)
     return pixel.b > 50;
   },
-  color: ({ x, y, image }) => "#61dafb",
+  color: ({ x, y, image }) => "#ffffff",
   radius: () => Math.random() * 1.5 + 0.5,
   mass: () => 40,
   friction: () => 0.15,
@@ -31,24 +30,38 @@ const motionForce = (x, y) => {
 export const Header = (props) => {
   const { innerWidth, innerHeight } = useWindowSize();
   const [entropy, setEntropy] = useState(500);
+  const [imgSrc, setImgSrc] = useState("img/about.png")
+  const [scale, setScale] = useState(2)
 
-  useEffect(() => setTimeout(() => setEntropy(20), 2000), []);
+  useEffect(() => setTimeout(() => setEntropy(30), 3000), [entropy]);
+  useEffect(() => setTimeout(() => {
+    if(imgSrc === "img/about.png") {
+      setScale(0.4);
+      setImgSrc("img/TN.png");
+      setEntropy(500);
+    }
+    else {
+      setScale(2);
+      setImgSrc("img/about.png")
+      setEntropy(500)
+    }
+  }, 10000), [imgSrc])
 
   return (
     <header id='header'>
       <div className='intro'>
-        {/* <ParticlesBg type="circle" bg={{zIndex: 0, position:"absolute", top:0}} /> */}
         <ParticleImage
-              src={"img/about.png"}
+              src={imgSrc}
               width={Number(innerWidth)}
               height={Number(innerHeight)}
-              scale={2}
+              scale={scale}
               entropy={entropy}
-              maxParticles={4200}
+              maxParticles={5000}
               particleOptions={particleOptions}
               mouseMoveForce={motionForce}
               touchMoveForce={motionForce}
               className={"background-react-component"}
+              backgroundColor="#000000"
             />
         <div className='overlay'>
           <div className='container'>
